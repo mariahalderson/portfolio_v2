1afe27841f09418bd98c537cb8212846f64678d7
Mariah Alderson <mariah@Mariahs-MBP.fanshawec.ca>
2019-03-01 14:49:54 -0500

    header component buil

--- js/templates/Header.js
+++ js/templates/Header.js
@@ -2,7 +2,28 @@ export default{
 
   template: /*html */ `
     <header>
-      <h1>The header</h1>
+      <div class="content-container">
+
+        <div class="logo-container">
+          <img src="assets/images/logo.svg">
+        </div>
+
+        <div id="hamburgerMenu" class="hamburger-container">
+          <div class="hamburger-line line-1"></div>
+          <div class="hamburger-line line-2"></div>
+          <div class="hamburger-line line-3"></div>
+        </div>
+
+        <nav id="mainNavigation">
+          <ul class="navigation">
+            <li><a href="#" class="navigation-link">home</a></li>
+            <li><a href="#" class="navigation-link">about</a></li>
+            <li><a href="#" class="navigation-link">portfolio</a></li>
+            <li><a href="#" class="navigation-link">resume</a></li>
+          </ul>
+        </nav>
+        
+      </div>
     </header>
   `,
 
